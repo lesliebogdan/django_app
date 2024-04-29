747e009f04491e09aabd9c3d1657be7e6db87147
leslie bogdan <leslie@lesliebogdan.com>
2024-04-29 17:48:52 +1000

    vb update

--- django_sample/other/views.py
+++ django_sample/other/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def simple_view(request):
-    return HttpResponse("<h1>HELLO bitchlet! This is a new web page</h1>")
+    return HttpResponse("<h1>HELLO bitchlet! This is a new web page</h1> <img src='vb.jpg'>>")
